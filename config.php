686ed2c138e56aec465d280a333b6d8f3da350fe
kenrick95 <kenrick95@gmail.com>
2015-09-13 22:33:57 +0800

    init, from DroidCare/dc; rough addresses

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * config.php
+ *
+ * This file contains configuration for the application
+ */
+
+// Database configuration
+$db_host = isset($_SERVER['OPENSHIFT_MYSQL_DB_HOST']) ?  $_SERVER['OPENSHIFT_MYSQL_DB_HOST'] : 'localhost';
+$db_user = isset($_SERVER['OPENSHIFT_MYSQL_DB_USERNAME']) ?  $_SERVER['OPENSHIFT_MYSQL_DB_USERNAME'] : 'root';
+$db_pass = isset($_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD']) ?  $_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD'] : '';
+$db_name = isset($_SERVER['OPENSHIFT_APP_NAME']) ?  $_SERVER['OPENSHIFT_APP_NAME'] : 'iderly';
+$db_port = isset($_SERVER['OPENSHIFT_MYSQL_DB_PORT']) ?  $_SERVER['OPENSHIFT_MYSQL_DB_PORT'] : 3306;
