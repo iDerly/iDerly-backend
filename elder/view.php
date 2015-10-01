41ad79255a5d2d9318c8f1fc701ab2d75eb2acae
kenrick95 <kenrick95@gmail.com>
2015-10-01 21:44:33 +0800

    elder profile now includes some game stats

--- elder/view.php
+++ elder/view.php
@@ -30,7 +30,7 @@ $this->respond('/[s:device_id]', function ($request, $response, $service, $app) 
     $error_msg = $service->flashes('error');
 
     if (is_empty($error_msg)) {
-        $sql_query = "SELECT `user_id`, `device_id`, `date_created`, `name`, `attachment` FROM `user` WHERE `device_id` = ?";
+        $sql_query = "SELECT `id`, `device_id`, `date_created`, `name`, `attachment` FROM `user` WHERE `device_id` = ?";
         $stmt = $mysqli->prepare($sql_query);
         if ($stmt) {
             $stmt->bind_param("s", $device_id);
@@ -38,6 +38,32 @@ $this->respond('/[s:device_id]', function ($request, $response, $service, $app) 
             $stmt->store_result();
             $stmt->bind_result($user_id, $device_id, $date_created, $name, $attachment);
             $stmt->fetch();
+
+            $game = [];
+
+            $sql_query = "SELECT MAX(`time_end`), MAX(`score`), AVG(`score`) FROM `game_result` WHERE `user_id` = ? AND `mode` = 'classic'";
+            $stmt = $mysqli->prepare($sql_query);
+            $stmt->bind_param("i", $user_id);
+            $res = $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($game['lastplayed_classic'], $game['hiscore_classic'], $game['avgscore_classic']);
+            $stmt->fetch();
+            $sql_query = "SELECT MAX(`time_end`), MAX(`score`), AVG(`score`) FROM `game_result` WHERE `user_id` = ? AND `mode` = 'unlimited'";
+            $stmt = $mysqli->prepare($sql_query);
+            $stmt->bind_param("i", $user_id);
+            $res = $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($game['lastplayed_unlimited'], $game['hiscore_unlimited'], $game['avgscore_unlimited']);
+            $stmt->fetch();
+            $sql_query = "SELECT MAX(`time_end`), MAX(`score`), AVG(`score`) FROM `game_result` WHERE `user_id` = ?";
+            $stmt = $mysqli->prepare($sql_query);
+            $stmt->bind_param("i", $user_id);
+            $res = $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($game['lastplayed'], $game['hiscore'], $game['avgscore']);
+            $stmt->fetch();
+
+
 
             $return_msg = array(
                 "user_id" => $user_id,
@@ -48,9 +74,12 @@ $this->respond('/[s:device_id]', function ($request, $response, $service, $app) 
                 "game_hiscore" => $game['hiscore'],
                 "game_hiscore_classic" => $game['hiscore_classic'],
                 "game_hiscore_unlimited" => $game['hiscore_unlimited'],
+                "game_lastplayed" => $game['lastplayed'],
                 "game_lastplayed_classic" => $game['lastplayed_classic'],
                 "game_lastplayed_unlimited" => $game['lastplayed_unlimited'],
-                "game_avgscore" => $game['avgscore']
+                "game_avgscore" => $game['avgscore'],
+                "game_avgscore_classic" => $game['avgscore_classic'],
+                "game_avgscore_unlimited" => $game['avgscore_unlimited']
             );
 
             $stmt->close();
